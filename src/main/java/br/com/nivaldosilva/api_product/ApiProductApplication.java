af498a966b58b0ec75eee89a4591420de35eebe2
Nivaldo Silva <nivaldosilva.contato@gmail.com>
2024-06-07 14:43:34 -0300

    Projeto API Restful de Produtos

--- src/main/java/br/com/nivaldosilva/api_product/ApiProductApplication.java
+++ src/main/java/br/com/nivaldosilva/api_product/ApiProductApplication.java
@@ -2,8 +2,10 @@ package br.com.nivaldosilva.api_product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApiProductApplication {
 
 	public static void main(String[] args) {
